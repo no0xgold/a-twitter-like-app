5b8a7bf7addda2e5aaecdd15b780418e40855043
nox_gold <noxraktor@gmail.com>
2020-12-12 14:50:44 +0330

    templates
    creating templates but didn't write anything special

--- tweets/views.py
+++ tweets/views.py
@@ -1,14 +1,27 @@
-from django.http import HttpResponse,Http404
+from django.http import HttpResponse,Http404,JsonResponse
 from django.shortcuts import render
 from .models import Tweet
 
 def home_view(request,*args,**kwargs):
     print(args,kwargs)
-    return HttpResponse("<h1>Hello<h1>")
-
+    #return HttpResponse("<h1>Hello<h1>")
+    return render(request,"pages/home.html", context={},status=200)
 def tweet_detail_view(request,tweet_id,*args,**kwargs):
+    """
+    REST API VIEW
+    Consme by JavaScript or Swift/Java/ios/Android
+    return json data
+    for now we use JS
+    """
+    data={
+        "id":tweet_id,
+    }
+    status=200
     try:
         obj=Tweet.objects.get(id=tweet_id)
+        data['content']=obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1>tweet {tweet_id}-{obj.content}<h1>")
+        data['message']="not found"
+        status=404
+    
+    return JsonResponse(data, status=status)
